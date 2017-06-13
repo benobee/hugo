ea1d97db0cb4be57d5f63a9363f5b855e0a892e8
Ben Obee <benobee@gmail.com>
2017-06-12 19:19:36 -0700

    new version

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
-import Scrollmap from 'scrollmap';
+// import Scrollmap from 'scrollmap';
 
-Scrollmap.trigger({ target: "main", surfaceVisible: 0.5 }, (el) => {
-	el.innerHTML = "HELLO";
-});
+// Scrollmap.trigger({ target: "main", surfaceVisible: 0.5 }, (el) => {
+// 	el.innerHTML = "HELLO";
+// });
 
-console.log(Scrollmap);
+// console.log(Scrollmap);
